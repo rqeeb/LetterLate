6580aa359cc7c85f3d5fc994b527aa67bfdf344f
RaqeebDev <raqeebauto@gmail.com>
2026-01-02 22:16:40 +0530

    db changes

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const DbUri = "mongodb+srv://raqeebpython_db_user:FPLnXyM5LJ_76UF@cluster0.mjgifhp.mongodb.net/letterDb";
+const DbUri = "";
 
 mongoose
   .connect(DbUri)
